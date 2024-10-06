873cce45ff003b566142264dc62c8a78c12623cb
Martin Heini <maheini35@me.com>
2024-10-06 03:49:31 +0200

    add scheduler for updates

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ RUN apt-get update && apt-get install -y \
     sh ./get-docker.sh
 
 # Create a non-root user
-ARG USER=dockeruser
+ARG USER=updater
 ARG UID=1000
 ARG GID=1000
 
@@ -16,6 +16,7 @@ RUN groupadd -g $GID $USER && \
     useradd -m -u $UID -g $GID -s /bin/bash $USER && \
     usermod -a -G docker $USER
 
+WORKDIR /home/$USER
 
 # Copy config folder and scripts content
 COPY --chown=$UID:$GID config ./config
@@ -24,19 +25,13 @@ COPY --chown=$UID:$GID scripts/* .
 # Switch to the non-root user
 USER $USER
 
-# Set the working directory
-WORKDIR /home/$USER
-
-
-# Copy config and scripts
-COPY ./config /config
-COPY ./scripts/* .
-
 # Set ENV
 ENV CUSTOM_TAG="1.0.0"
 ENV CUSTOM_IMAGE=""
 ENV DB_PASSWORD=""
 ENV PROJECT_NAME=""
+ENV TZ="UTC"
+ENV SCHEDULED_TIME=""
 
 # Entry point
-ENTRYPOINT ["/bin/bash", "update.sh"]
+ENTRYPOINT ["/bin/bash", "scheduler.sh"]
---
+++ scripts/scheduler.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ ! -n "${SCHEDULER_TIME}" ]; then
+    echo "No time schedule selected - proceeding with a instant run..."
+    /bin/bash update.sh
+    exit 0
+fi
+
+# Scheduler loop
+while true
+do
+    # Check if any error occurs during date test conversion
+    date -d "${SCHEDULER_TIME}" "+%Y-%m-%d" >/dev/null 2>&1
+	if [ ! $? -eq 0 ]; then
+        echo "Error: Time-schedule is invalid"
+        exit 1
+    fi
+
+    # Compare both dates in seconds
+    difference=$(($(date -d "${SCHEDULER_TIME}" +%s) - $(date +%s)))
+    # Check if timestamp is past 24h - if so, add 
+    if [ $difference -lt 0 ]; then
+        difference=$((difference + 86400))  # Add 24h -> schedule for tomorrow
+    fi
+
+    # Check if difference is valid (in the future) now  -> "difference >= 0"
+    if [ $difference -ge 0 ]; then
+        seconds=$((difference + $(date +%s)))
+        echo "Waiting for next schedule on: $(date -d @$seconds +"%Y-%m-%d %H:%M:%S")"
+        
+        sleep $((difference))   # Sleep until timestamp matches
+        echo "Starting scheduled update at ${date}"
+        /bin/bash update.sh
+    else
+        echo "Error: Time-schedule cant be read as future timestamp"
+        exit 1
+    fi
+done
+
